e2c0537d840bf3dc6d26a08bb85e4c49b5c80c86
Domas <domasx2@gmail.com>
2011-06-20 22:49:52 +0300

    loading screen redone properly

--- main.js
+++ main.js
@@ -4,40 +4,48 @@ var settings=require('./settings');
 var skin=require('./skin');
 
 
-gamejs.preload(combatracer.getPreloadList());
+
 gamejs.display.setCaption("Bandit Racer");
 var t;
-
-var progfn=gamejs.ready(function(){
-   
-   var game=new combatracer.Game();
-   if(t)clearInterval(t);
-   game.start();   
-});
-
+var progfn;
 var canvas=null;
 var display=null;
 var font=new gamejs.font.Font(skin.fonts.loading[0]);
 var loading_img=font.render('Loading...', skin.fonts.loading[1]);
 var loading_img_size=loading_img.getSize();
-function loading(){
-  if(!canvas){
-      canvas=document.getElementById("jsgamecanvas");
-      if(canvas){
-         display=gamejs.display.setMode([settings.get('SCREEN_WIDTH'), settings.get('SCREEN_HEIGHT')]);
-      }
-  }else{
-      var display_size=display.getSize()
-      gamejs.draw.rect(display, skin.ui_background, new gamejs.Rect([0, 0], display_size));
-      var loading_pt=[display_size[0]/2-loading_img_size[0]/2, display_size[1]/2-loading_img_size[1]/2];
-      display.blit(loading_img, loading_pt);
+
+function loadTick(){
+
+   var display_size=display.getSize()
+   gamejs.draw.rect(display, skin.ui_background, new gamejs.Rect([0, 0], display_size));
+   var loading_pt=[display_size[0]/2-loading_img_size[0]/2, display_size[1]/2-loading_img_size[1]/2-100];
+   display.blit(loading_img, loading_pt);
+   if(progfn){
       var progress=progfn();
       gamejs.draw.rect(display, 'black', new gamejs.Rect([loading_pt[0]-100, loading_pt[1]+50], [loading_img_size[0]+200, 40]), 2);
       gamejs.draw.rect(display, 'black', new gamejs.Rect([loading_pt[0]-100, loading_pt[1]+50], [(loading_img_size[0]+200)*progress, 40]), 0);
       
-  }
+   }
   
 };
 
-t=setInterval(loading, 200)
-loading();
+
+function init(){
+   display=gamejs.display.setMode([settings.get('SCREEN_WIDTH'), settings.get('SCREEN_HEIGHT')]);
+   loadTick();
+   gamejs.preload(combatracer.getPreloadList());
+   progfn=gamejs.ready(function(){
+      var game=new combatracer.Game();
+      if(t)clearInterval(t);
+      game.start(display);   
+   });
+   t=setInterval(loadTick, 50);
+   
+}
+
+window.onload=init;
+
+
+
+
+
