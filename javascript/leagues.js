7aee513fc81d2540809e28fa70b505b241072b5d
Domas <domasx2@gmail.com>
2011-09-09 00:59:31 +0300

    2 new tracks

--- javascript/leagues.js
+++ javascript/leagues.js
@@ -1,9 +1,9 @@
 exports[0]={'name':'Begginer League',
-            'tracks':['drycircuit', 'snowpeakcircuit'],
+            'tracks':['drycircuit', 'snowpeakcircuit', 'greenring', 'frogfoot'],
             'bots':['Bob', 'CatLady', 'Dominator'],
             'reward':1000};
             
-exports[1]={'name':'Rookie League',
+exports[1]={'name':'Veteran League',
             'tracks':['spiralraceway', 'deathvalley'],
             'bots':['HotShot', 'TheDude', 'Ratman'],
             'reward':2500};
