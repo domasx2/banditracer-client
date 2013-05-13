8d547a244d5428c332ce5a89074e0324f987a145
Domas <domasx2@gmail.com>
2013-05-13 20:50:11 +0300

    just load precompiled levels

--- main.js
+++ main.js
@@ -3,20 +3,5 @@ exports.settings = require('./javascript/settings');
 exports.car_descriptions = require('./javascript/car_descriptions');
 exports.combatracer=require('./javascript/combatracer');
 exports.cars=require('./javascript/cars');
-
-
-var LEVELS_PATH = './levels/';
-var levels = exports.levels = [];
-var fs=require('fs');
-if(fs && fs.readdirSync){
-  var dirName = __dirname +'/' + LEVELS_PATH;
-  fs.readdirSync(dirName).forEach(function(fname) {
-     var levelname=fname.split('.')[0];
-     var content=fs.readFileSync(dirName+fname, 'utf-8');
-     levels[levelname]=JSON.parse(content);
-  });
-} else {
-  var l = require('./javascript/levels');
-  l.init();
-  exports.levels = l.all;
-}
+var levels = exports.levels = require('./javascript/levels');
+levels.init();
